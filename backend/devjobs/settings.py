27160846869e8342295312a1c7777cbb54b785ce
luig-desktop <luisgardaz13@gmail.com>
2026-07-08 23:43:49 -0500

    chore: quitando lo que sobreescribia los database de arriba

--- backend/devjobs/settings.py
+++ backend/devjobs/settings.py
@@ -78,16 +78,6 @@ TEMPLATES = [
   },
 ]
 WSGI_APPLICATION = 'devjobs.wsgi.application'
-DATABASES = {
-  'default': {
-    'ENGINE': 'django.db.backends.postgresql',
-    'NAME': os.getenv('POSTGRES_DB'),
-    'USER': os.getenv('POSTGRES_USER'),
-    'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
-    'HOST': os.getenv('POSTGRES_HOST', 'localhost'),
-    'PORT': os.getenv('POSTGRES_PORT', '5432'),
-  }
-}
 AUTH_PASSWORD_VALIDATORS = [
   {
     'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
