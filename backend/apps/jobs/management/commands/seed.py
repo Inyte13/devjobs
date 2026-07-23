336b058ae45a8c34780e385758cff632a7df6575
luig-desktop <luisgardaz13@gmail.com>
2026-07-23 13:21:38 -0500

    chore: modificando la seed

--- backend/apps/jobs/management/commands/seed.py
+++ backend/apps/jobs/management/commands/seed.py
@@ -2,7 +2,7 @@ from apps.jobs.models.company import Company
 from apps.jobs.models.location import Location
 from apps.jobs.models.technology import Technology
 from apps.jobs.models.user import User
-from django.core.management.base import BaseCommand
+from django.core.management.base import BaseCommand, CommandError
 from django.db import transaction
 
 
@@ -11,40 +11,33 @@ class Command(BaseCommand):
 
   @transaction.atomic
   def handle(self, *args, **options):
-    admin, created = User.objects.get_or_create(
-      username='luisg',
-      defaults={
-        'email': 'luisgardaz13@gmail.com',
-        'is_staff': True,
-        'is_superuser': True, 
-      },
-    )
-    if created:
-      admin.set_password('1234')
-      admin.creator_id = admin.id  # type: ignore
-      admin.modifier_id = admin.id  # type: ignore
-      admin.save()
+    admin = User.objects.filter(is_superuser=True).first()
+    if admin is None:
+      raise CommandError('No existe un superusuario')
 
-    companies = ['Globant', 'Mercado Libre', 'Rappi', 'NTT Data']
+    companies = ['GLOBANT', 'MERCADO LIBRE', 'RAPPI', 'NTT DATA']
     for name in companies:
       Company.objects.get_or_create(
-        name=name, defaults={'creator_id': admin.id, 'modifier_id': admin.id}
+        name=name,
+        defaults={'creator_id': admin.id, 'modifier_id': admin.id},
       )
 
     locations = [
-      'Lima, Perú',
-      'Buenos Aires, Argentina',
-      'Bogotá, Colombia',
+      'LIMA, PERÚ',
+      'BUENOS AIRES, ARGENTINA',
+      'BOGOTÁ, COLOMBIA',
     ]
     for name in locations:
       Location.objects.get_or_create(
-        name=name, defaults={'creator_id': admin.id, 'modifier_id': admin.id}
+        name=name,
+        defaults={'creator_id': admin.id, 'modifier_id': admin.id},
       )
 
-    technologies = ['Python', 'Django', 'React', 'TypeScript', 'PostgreSQL']
+    technologies = ['PYTHON', 'DJANGO', 'REACT', 'TYPESCRIPT', 'POSTGRESQL']
     for name in technologies:
       Technology.objects.get_or_create(
-        name=name, defaults={'creator_id': admin.id, 'modifier_id': admin.id}
+        name=name,
+        defaults={'creator_id': admin.id, 'modifier_id': admin.id},
       )
 
     self.stdout.write(self.style.SUCCESS('Base de datos poblada con éxito'))
